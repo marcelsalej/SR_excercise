5c5e706ccf5ee09b898772c40895bed0e8c1423c
Marcel Šalej <marcel.salej@icloud.com>
2022-10-12 11:33:30 +0200

    Initial xcode project commit

---
+++ VolumeControl/VolumeControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolumeControl
+//
+//  Created by Marcel Salej on 12/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VolumeControl/VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by Marcel Salej on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
